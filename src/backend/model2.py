2aeba48b2e0df8c29416a34a1fac8b72183ca5d5
NeftaliGC <aliguiacruz@outlook.com>
2025-02-19 19:20:25 -0600

    clase del modelo 2

--- src/backend/model2.py
+++ src/backend/model2.py
@@ -4,80 +4,142 @@ import pandas as pd
 import time
 import json
 import os
+import sys
+import io
 
-# Abrir json
-with open('src/backend/data/datosm2.json', 'r') as file:
-    data = json.load(file)
+class Model2:
+    def __init__(self):
+        self.token = None
+        self.data = None
+        self.n = 0
+        self.m = 0
+        self.num_profesores = 0
+        self.disp = None
+        self.gp = None
+        self.asig = None
+        self.objective = None
+        self.constraints = None
+        self.model = None
+        self.solution = None
+        self.valOfSol = 0
 
-name = "ej50"
+    def start_solution(self, token = None, data = None):
+        if token is not None:
+            self.token = token
+            # Abrir datos.json
+            with open(f"src/backend/data/{token}.json", "r") as f:
+                # Cargar el contenido completo del archivo JSON
+                dat = json.load(f)
+                
+                # Acceder a la parte específica de "Disponibilidad Dias"
+                disponibilidad_horario = dat["input"][0]["data"]["Disponibilidad Horario"]
+                self.data = disponibilidad_horario
 
-for i in data:
-    if i['id'] == name:
-        n, m = i['DISP']['shape'] # n (numero de grupos), m (numero de horarios)
-        num_profesores = i['GP']['shape'][1]
-        DISP = np.array(i['DISP']['data'])
-        GP = np.array(i['GP']['data'])
+        if data is not None:
+            self.data = data
 
-print("INICIO")
-# Medir el tiempo de ejecución
-start_time_ex = time.time()  # Inicia el temporizador
+    # Paso 1
+    def setVariables(self):
+        # Obtén el valor de "DISP"
+        self.n, self.m = self.data["DISP"]["shape"]
+        self.disp = np.array(self.data["DISP"]["data"])
 
-# Variables de decisión: asignación de grupos a días
-ASIG = cp.Variable((n, m), boolean=True)
+        # Obtén el valor de "GP"
+        self.num_profesores = self.data["GP"]["shape"][1]
+        self.gp = np.array(self.data["GP"]["data"])
 
-# Función objetivo cuadrática
-def objective_function():
-    sumas = [cp.sum(ASIG[:, j]) for j in range(m)]
-    return  cp.sum([(sumas[0] - sumas[j])**2 for j in range(1, m-1)]) + \
-            cp.sum([(sumas[j1] - sumas[j2])**2 for j1 in range(m-1) for j2 in range(j1+1, m-1)]) + \
-            cp.sum([(sumas[j] - sumas[m-1])**2 for j in range(m-1)])
+        # Variables de decisión: asignación de grupos a días
+        self.asig = cp.Variable((self.n, self.m), boolean=True)
 
-objective = cp.Minimize(objective_function())
+        # Configura el objetivo
+        self.objective = cp.Minimize(self.objective_function())
 
-# Restricciones
-constraints = [
-    # Restricción de número de horarios (sesiones) asignados por día
-    cp.sum(ASIG[i, :]) == 1 for i in range(n)
-]
+    # Paso 2
+    def setConstraints(self):
+        self.constraints = [
+            # Restricción de número de horarios (sesiones) asignados por día
+            cp.sum(self.asig[i, :]) == 1 for i in range(self.n)
+        ]
 
-# Restricción para asegurar que un profesor no esté asignado a más de un grupo por horario
-for prof in range(num_profesores):
-    grupos_profesor = [grupo for grupo in range(n) if GP[grupo, prof] == 1]  # Grupos asignados al profesor
-    for hora in range(m):
-        constraints.append(
-            cp.sum([ASIG[grupo, hora] for grupo in grupos_profesor]) <= 1
-        )
+        # Restricción para asegurar que un profesor no esté asignado a más de un grupo por horario
+        for prof in range(self.num_profesores):
+            grupos_profesor = [grupo for grupo in range(self.n) if self.gp[grupo, prof] == 1]
+            for hora in range(self.m):
+                self.constraints.append(
+                    cp.sum([self.asig[grupo, hora] for grupo in grupos_profesor]) <= 1
+                )
 
-# Restriccion de disponibilidad de horarios por grupo
-constraints += [
-    ASIG[i, j] <= DISP[i, j] for i in range(n) for j in range(m)
-]
+        # Restricción de disponibilidad de horarios por grupo
+        self.constraints += [
+            self.asig[i, j] <= self.disp[i, j] for i in range(self.n) for j in range(self.m)
+        ]
+
+    # Paso 3
+    def setModel(self):
+        # Crear el modelo
+        self.model = cp.Problem(self.objective, self.constraints)
+
+    # Paso 4
+    def solveModel(self):
+        # Redirigir stdout a un buffer
+        old_stdout = sys.stdout  # Guarda la salida estándar original
+        buffer = io.StringIO()  # Crea un buffer en memoria
+        sys.stdout = buffer  # Redirige stdout al buffer
+
+        try:
+            # Resolver el problema
+            self.model.solve(solver=cp.GUROBI, verbose=True)
+            self.valOfSol = self.model.value
+        finally:
+            # Restaurar stdout
+            sys.stdout = old_stdout
+        
+        # Guardar el verbose capturado en un archivo
+        with open(f"src/backend/res/{self.token}", "w") as f:
+            f.write(buffer.getvalue())
+
+    # Función objetivo cuadrática
+    def objective_function(self):
+        sumas = [cp.sum(self.asig[:, j]) for j in range(self.m)]
+        return  cp.sum([(sumas[0] - sumas[j])**2 for j in range(1, self.m-1)]) + \
+                cp.sum([(sumas[j1] - sumas[j2])**2 for j1 in range(self.m-1) for j2 in range(j1+1, self.m-1)]) + \
+                cp.sum([(sumas[j] - sumas[self.m-1])**2 for j in range(self.m-1)])
+
+    def get_solution(self):
+        print("Asignación de grupos a horarios:")
+        # Imprimir matriz de solución binaria
+        solution_matrix = np.zeros((self.n, self.m))
+        for i in range(self.n):
+            for j in range(self.m):
+                if self.asig.value[i, j] > 0.5:
+                    solution_matrix[i, j] = 1
+
+        return solution_matrix
 
 
-# Crear el modelo
-model = cp.Problem(objective, constraints)
+if __name__ == "__main__":
+    with open('src/backend/data/datosm2.json', 'r') as file:
+        data = json.load(file)
 
-# Resolver el problema
-model.solve(solver=cp.GUROBI, verbose=True)
+    name = "ej10-5"
 
-end_time_ex = time.time() # Detiene el temporizador
+    for i in data:
+        if i['id'] == name:
+            data = i
 
-# Imprimir los resultados
-print("Asignación de grupos a horarios:")
-# Imprimir matriz de solución binaria
-solution_matrix = np.zeros((n, m))
-for i in range(n):
-    for j in range(m):
-        if ASIG.value[i, j] > 0.5:
-            solution_matrix[i, j] = 1
+    model = Model2()
+    model.start_solution(data=data)
+    model.setVariables()
+    model.setConstraints()
+    model.setModel()
+    model.solveModel()
 
-print(solution_matrix)
+    solution = model.get_solution()
 
-print(f"tiempo de ejecucion: {end_time_ex - start_time_ex}")
+    print(solution)
 
+    # Exportar en csv
+    hora_inicio = 7 #numerico 00:00 - 24:00
 
-# Exportar en csv
-hora_inicio = 7 #numerico 00:00 - 24:00
-
-df_solution = pd.DataFrame(solution_matrix, columns=[f'Horario: {hora_inicio + 2 * j}:00 - {hora_inicio + 2 * (j + 1)}:00' for j in range(m)])
-df_solution.to_csv('solucion_binaria.csv', index=False)
+    df_solution = pd.DataFrame(solution, columns=[f'Horario: {hora_inicio + 2 * j}:00 - {hora_inicio + 2 * (j + 1)}:00' for j in range(model.m)])
+    df_solution.to_csv('src/backend/res/solucion_binaria.csv', index=False)
